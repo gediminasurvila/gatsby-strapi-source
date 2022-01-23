be2c32b17d74c35e087341b8f59f08c79949bbf8
Gediminas Survila <gsurvila@Gediminass-Mac-mini.local>
2022-01-23 16:40:38 +0200

    strapi source

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,19 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: "https://gerapatirtis.survila.art",
+        collectionTypes: [
+          "post",
+          "category",
+          "tag"
+        ],
+        queryLimit: 1000,
+      },
+    },
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
