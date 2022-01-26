955f43385c8b74e256feb463ca21f2243e3c0548
Gediminas Survila <gediminas.survila@hostinger.com>
2022-01-26 20:56:03 +0200

    plugin added

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,17 +35,25 @@ module.exports = {
       resolve: "gatsby-source-strapi",
       options: {
         apiURL: "https://gerapatirtis.survila.art",
-        collectionTypes: [
-          "post",
-          "category",
-          "tag"
-        ],
+        collectionTypes: ["post", "category", "tag"],
         queryLimit: 1000,
       },
     },
-    
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
