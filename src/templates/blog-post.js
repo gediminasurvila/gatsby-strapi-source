4875f052cd5d6dde61c7f8497212d6aadb499b58
Gediminas Survila <gsurvila@Gediminass-Mac-mini.local>
2022-01-28 18:08:09 +0200

    post page refactored

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,42 +1,39 @@
 import * as React from "react";
 import { graphql } from "gatsby";
-import Img from "gatsby-image"
+import Img from "gatsby-image";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 
-const BlogPost = props => {
-  const post = props.data.allStrapiPost.nodes[0];
-
+const BlogPost = ({ data }) => {
+  const post = data.strapiPost;
   return (
     <Layout>
       <Seo title={post.Title} />
       <h1>{post.Title}</h1>
-      <Img fluid={post?.remoteImage?.childImageSharp.fluid} />
+      <Img fluid={post?.CoverImage?.localFile?.childImageSharp.fluid} />
     </Layout>
   );
 };
 
 export const query = graphql`
   query ($id: String!) {
-    allStrapiPost(filter: { id: { eq: $id } }) {
-      nodes {
-        Title
-        id
-        categories {
-          Name
-          id
-        }
-        Body
-        published_at
-        updated_at
-        created_at
-        remoteImage {
+    strapiPost(id: { eq: $id }) {
+      id
+      CoverImage {
+        localFile {
           childImageSharp {
             fluid(maxWidth: 1200) {
               ...GatsbyImageSharpFluid
             }
           }
         }
+      }
+      Body
+      Title
+      published_at
+      categories {
+        Name
+        id
       }
     }
   }
